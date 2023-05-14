2a5b194834b39a4f4d455991219476be33c9d0c2
noliiva <laure.ivanovitch@gmail.com>
2023-05-14 15:36:30 +0200

    fix context type

--- src/components/BoxRDV/RDVContext.tsx
+++ src/components/BoxRDV/RDVContext.tsx
@@ -1,13 +1,13 @@
 import { createContext, useContext, useState, useMemo } from "react";
 import type { PropsWithChildren, Dispatch, SetStateAction } from "react";
 
-type Value = {
+type RDVContextValue = {
   client?: string;
   handleClientChange: Dispatch<SetStateAction<string>>;
 };
 
-const RDVContext = createContext<Value | null>(null);
-export const useRDVContext = () => useContext(RDVContext);
+const RDVContext = createContext<RDVContextValue | null>(null);
+export const useRDVContext = () => useContext(RDVContext) as RDVContextValue;
 
 export function RDVProvider({ children }: PropsWithChildren) {
   const [client, setClient] = useState("");
